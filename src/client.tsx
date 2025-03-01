7bdb5a5bbf9359cf6326243d78b44791dcd569a3
Brenley Dueck <brenleydueck@gmail.com>
2025-03-01 10:24:11 -0600

    Integrate tanstack solid-start

---
+++ src/client.tsx
@@ -0,0 +1,8 @@
+import { StartClient } from '@tanstack/solid-start'
+
+import { hydrate } from 'solid-js/web'
+import { createRouter } from './router'
+
+const router = createRouter()
+
+hydrate(() => <StartClient router={router} />, document.body)
